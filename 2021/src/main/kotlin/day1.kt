1ceb0876aac0baa6aa3878d8e19f3c5518c43012
dunyakirkali <dunyakirkali@gmail.com>
2022-11-11 20:44:42 +0100

    Day 1

---
+++ 2021/src/main/kotlin/day1.kt
@@ -0,0 +1,38 @@
+import java.io.File
+
+val input =
+    File("src/main/resources/day1/input.txt")
+        .readText(Charsets.UTF_8)
+        .split('\n')
+        .map {
+            it.toInt()
+        }
+
+fun part1(): Int {
+    return input
+        .windowed(2, 1, false)
+        .map {
+            it[1] - it[0]
+        }.filter {
+            it > 0
+        }.size
+}
+
+fun part2(): Int {
+    return input
+        .windowed(3, 1, false)
+        .map {
+            it.sum()
+        }
+        .windowed(2, 1, false)
+        .map {
+            it[1] - it[0]
+        }.filter {
+            it > 0
+        }.size
+}
+
+fun main() {
+    println("part 1 = ${part1()}")
+    println("part 2 = ${part2()}")
+}
