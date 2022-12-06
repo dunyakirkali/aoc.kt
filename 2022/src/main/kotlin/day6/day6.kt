8df9efe2ce7a57b95e3e9f9e6cb8089207182687
dunyakirkali <dunyakirkali@gmail.com>
2022-12-06 06:23:16 +0100

    2022 Day 6

---
+++ 2022/src/main/kotlin/day6/day6.kt
@@ -0,0 +1,51 @@
+package day6
+
+import java.io.File
+import kotlin.streams.toList
+
+val input: String =
+    File("src/main/resources/day6/input.txt")
+        .readText(Charsets.UTF_8)
+
+fun part1(): Int? {
+    return input
+        .chunked(1)
+        .windowed(4, 1)
+        .withIndex()
+        .map { (ind, grp) ->
+            grp
+                .distinct()
+                .count()
+                .let { cnt ->
+                    Triple(grp, cnt, ind + 4)
+                }
+        }
+        .find { (_, cnt, _) ->
+            cnt == 4
+        }
+        ?.third
+}
+
+fun part2(): Int? {
+    return input
+        .chunked(1)
+        .windowed(14, 1)
+        .withIndex()
+        .map { (ind, grp) ->
+            grp
+                .distinct()
+                .count()
+                .let { cnt ->
+                    Triple(grp, cnt, ind + 14)
+                }
+        }
+        .find { (_, cnt, _) ->
+            cnt == 14
+        }
+        ?.third
+}
+
+fun main() {
+    println("part 1 = ${part1()}")
+    println("part 2 = ${part2()}")
+}
